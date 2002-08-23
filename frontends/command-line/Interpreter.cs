d5c2465d5c9c62dda714f0130ac95a4b72a6e42c
Martin Baulig <martin@novell.com>
2002-08-23 15:35:18 +0000

    Made this a general command interpreter.
    svn path=/trunk/debugger/; revision=6951

--- frontends/command-line/Interpreter.cs
+++ frontends/command-line/Interpreter.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Text;
-using System.Reflection;
+using System.IO;
 using System.Threading;
+using System.Reflection;
 using Mono.Debugger;
 using Mono.Debugger.Backends;
 using Mono.Debugger.Languages;
@@ -13,34 +14,57 @@ namespace Mono.Debugger.Frontends.CommandLine
 	/// </summary>
 	public class Interpreter
 	{
-		GDB backend;
+		IDebuggerBackend backend;
+		TextWriter stdout, stderr;
 
-		Interpreter (string application, string[] arguments)
+		// <summary>
+		//   Create a new command interpreter for the debugger backend @backend.
+		//   The interpreter sends its stdout to @stdout and its stderr to @stderr.
+		// </summary>
+		public Interpreter (IDebuggerBackend backend, TextWriter stdout, TextWriter stderr)
 		{
-			backend = new GDB (application, arguments);
-
-			backend.TargetOutput += new TargetOutputHandler (TargetOutput);
-			backend.TargetError += new TargetOutputHandler (TargetError);
-			backend.StateChanged += new StateChangedHandler (StateChanged);
-
-			backend.SourceFileFactory = new SourceFileFactory ();
+			this.backend = backend;
+			this.stdout = stdout;
+			this.stderr = stderr;
 		}
 
-		void ShowHelp ()
+		public void ShowHelp ()
 		{
-			Console.WriteLine ("Commands:");
-			Console.WriteLine ("  q, quit,exit     Quit the debugger");
-			Console.WriteLine ("  r, run           Start the target");
-			Console.WriteLine ("  c, continue      Continue the target");
-			Console.WriteLine ("  abort            Abort the target");
-			Console.WriteLine ("  kill             Kill the target");
-			Console.WriteLine ("  b, break-method  Add breakpoint for a CSharp method");
-			Console.WriteLine ("  !, gdb           Send command to gdb");
+			stderr.WriteLine ("Commands:");
+			stderr.WriteLine ("  q, quit,exit     Quit the debugger");
+			stderr.WriteLine ("  r, run           Start the target");
+			stderr.WriteLine ("  c, continue      Continue the target");
+			stderr.WriteLine ("  abort            Abort the target");
+			stderr.WriteLine ("  kill             Kill the target");
+			stderr.WriteLine ("  b, break-method  Add breakpoint for a CSharp method");
+			stderr.WriteLine ("  f, frame         Get current stack frame");
+			stderr.WriteLine ("  s, step          Single-step");
+			stderr.WriteLine ("  n, next          Single-step");
+			stderr.WriteLine ("  !, gdb           Send command to gdb");
 		}
 
-		StringBuilder output_builder = null;
+		// <summary>
+		//   Process one command and return true if we should continue processing
+		//   commands, ie. until the "quit" command has been issued.
+		// </summary>
+		public bool ProcessCommand (string line)
+		{
+			if (line == "")
+				return true;
 
-		bool DoOneCommand (string command, string[] args)
+			string[] tmp_args = line.Split (' ', '\t');
+			string[] args = new string [tmp_args.Length - 1];
+			Array.Copy (tmp_args, 1, args, 0, tmp_args.Length - 1);
+			string command = tmp_args [0];
+
+			return ProcessCommand (tmp_args [0], args);
+		}
+
+		// <summary>
+		//   Process one command and return true if we should continue processing
+		//   commands, ie. until the "quit" command has been issued.
+		// </summary>
+		public bool ProcessCommand (string command, string[] args)
 		{
 			switch (command) {
 			case "h":
@@ -51,7 +75,6 @@ namespace Mono.Debugger.Frontends.CommandLine
 			case "q":
 			case "quit":
 			case "exit":
-				backend.Quit ();
 				return false;
 
 			case "r":
@@ -62,6 +85,21 @@ namespace Mono.Debugger.Frontends.CommandLine
 			case "c":
 			case "continue":
 				backend.Continue ();
+				break;
+
+			case "f":
+			case "frame":
+				backend.Frame ();
+				break;
+
+			case "s":
+			case "step":
+				backend.Step ();
+				break;
+
+			case "n":
+			case "next":
+				backend.Next ();
 				break;
 
 			case "abort":
@@ -79,42 +117,42 @@ namespace Mono.Debugger.Frontends.CommandLine
 			case "b":
 			case "break-method": {
 				if (args.Length != 2) {
-					Console.WriteLine ("Command requires an argument");
+					stderr.WriteLine ("Command requires an argument");
 					break;
 				}
 
 				ILanguageCSharp csharp = backend as ILanguageCSharp;
 				if (csharp == null) {
-					Console.WriteLine ("Debugger doesn't support C#");
+					stderr.WriteLine ("Debugger doesn't support C#");
 					break;
 				}
 
 				Type type = csharp.CurrentAssembly.GetType (args [0]);
 				if (type == null) {
-					Console.WriteLine ("No such type: `{0}'", args [0]);
+					stderr.WriteLine ("No such type: `" + args [0] + "'");
 					break;
 				}
 
 				MethodInfo method = type.GetMethod (args [1]);
 				if (method == null) {
-					Console.WriteLine ("Can't find method `{0}' in type `{1}'",
-							   args [1], args [0]);
+					stderr.WriteLine ("Can't find method `" + args [1] + "' in type `" +
+							  args [0] + "'");
 					break;
 				}
 
 				ITargetLocation location = csharp.CreateLocation (method);
 				if (location == null) {
-					Console.WriteLine ("Can't get location for method: {0}.{1}",
-							   args [0], args [1]);
+					stderr.WriteLine ("Can't get location for method: " +
+							  args [0] + "." + args [1]);
 					break;
 				}
 
 				IBreakPoint break_point = backend.AddBreakPoint (location);
 
 				if (break_point != null)
-					Console.WriteLine ("Added breakpoint: " + break_point);
+					stderr.WriteLine ("Added breakpoint: " + break_point);
 				else
-					Console.WriteLine ("Unable to add breakpoint!");
+					stderr.WriteLine ("Unable to add breakpoint!");
 
 				break;
 			}
@@ -123,120 +161,21 @@ namespace Mono.Debugger.Frontends.CommandLine
 			case "gdb": {
 				GDB gdb = backend as GDB;
 				if (gdb == null) {
-					Console.WriteLine ("This command is only available when using " +
-							   "gdb as backend");
+					stderr.WriteLine ("This command is only available when using " +
+							  "gdb as backend");
 					break;
 				}
 
-				output_builder = new StringBuilder ();
 				gdb.SendUserCommand (String.Join (" ", args));
-				Console.WriteLine (output_builder.ToString ());
-				output_builder = null;
 				break;
 			}
 
 			default:
-				Console.WriteLine ("Unknown command: " + command);
+				stderr.WriteLine ("Unknown command: " + command);
 				break;
 			}
 
 			return true;
-		}
-
-		void TargetOutput (string output)
-		{
-			if (output_builder != null)
-				output_builder.Append (output);
-			else
-				Console.WriteLine ("OUTPUT: |" + output + "|");
-		}
-
-		void TargetError (string output)
-		{
-			Console.WriteLine ("ERROR : |" + output + "|");
-		}
-
-		void StateChanged (TargetState new_state)
-		{
-			Console.WriteLine ("STATE CHANGED: " + new_state);
-		}
-
-		public void MainLoop ()
-		{
-			bool keep_running = true;
-
-			while (keep_running) {
-				Console.Write ("$ ");
-
-				string line = Console.ReadLine ();
-				if (line == null)
-					break;
-
-				if (line == "")
-					continue;
-
-				string[] args = line.Split (' ', '\t');
-				string[] new_args = new string [args.Length - 1];
-				Array.Copy (args, 1, new_args, 0, args.Length - 1);
-
-				keep_running = DoOneCommand (args [0], new_args);
-			}
-
-			backend.Dispose ();
-		}
-
-		//
-		// IDisposable
-		//
-
-		private bool disposed = false;
-
-		protected virtual void Dispose (bool disposing)
-		{
-			// Check to see if Dispose has already been called.
-			if (!this.disposed) {
-				// If this is a call to Dispose,
-				// dispose all managed resources.
-				if (disposing) {
-					// Do stuff here
-					backend.Dispose ();
-				}
-				
-				// Release unmanaged resources
-				this.disposed = true;
-			}
-		}
-
-		public void Dispose ()
-		{
-			Dispose (true);
-			// Take yourself off the Finalization queue
-			GC.SuppressFinalize (this);
-		}
-
-		~Interpreter ()
-		{
-			Dispose (false);
-		}
-
-		//
-		// Main
-		//
-		static void Main (string[] args)
-		{
-			if (args.Length < 1) {
-				Console.WriteLine ("Usage: {0} application.exe [args]",
-						   AppDomain.CurrentDomain.FriendlyName);
-				Environment.Exit (1);
-			}
-
-			string[] new_args = new string [args.Length - 1];
-			Array.Copy (args, 1, new_args, 0, args.Length - 1);
-
-			Interpreter interpreter = new Interpreter (args [0], new_args);
-
-			interpreter.MainLoop ();
-			interpreter.Dispose ();
 		}
 	}
 }
