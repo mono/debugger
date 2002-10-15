b5d7b7b1f3e69821646ad94472301ab242a76762
Martin Baulig <martin@novell.com>
2002-10-15 13:11:05 +0000

    Added another foreach example.
    svn path=/trunk/debugger/; revision=8284

--- test/C.cs
+++ test/C.cs
@@ -9,8 +9,20 @@ class X
 		Console.WriteLine ("Done");
 	}
 
+	static long Test (params int[] args)
+	{
+		long sum = 0;
+		foreach (int arg in args) {
+			Console.WriteLine (arg);
+			sum += arg;
+		}
+		Console.WriteLine ("Done");
+		return sum;
+	}
+
 	static void Main ()
 	{
 		Test ("New York", "Boston", "Ximian");
+		Console.WriteLine (Test (1,2,3,4,5,6,7,8));
 	}
 }
