96eecd8cf88e867a53bb86e28b2252907e564c07
Martin Baulig <martin@novell.com>
2002-10-16 23:53:14 +0000

    New test app.
    svn path=/trunk/debugger/; revision=8332

---
+++ test/D.cs
@@ -0,0 +1,76 @@
+using System;
+
+class X
+{
+	static void Simple ()
+	{
+		int a = 5;
+		long b = 7;
+		float f = (float) a / (float) b;
+
+		string hello = "Hello World";
+
+		Console.WriteLine (a);
+		Console.WriteLine (b);
+		Console.WriteLine (f);
+		Console.WriteLine (hello);
+	}
+
+	static void BoxedValueType ()
+	{
+		int a = 5;
+		object boxed_a = a;
+
+		Console.WriteLine (a);
+		Console.WriteLine (boxed_a);
+	}
+
+	static void BoxedReferenceType ()
+	{
+		string hello = "Hello World";
+		object boxed_hello = hello;
+
+		Console.WriteLine (hello);
+		Console.WriteLine (boxed_hello);
+	}
+
+	static void SimpleArray ()
+	{
+		int[] a = { 3, 4, 5 };
+
+		Console.WriteLine (a [2]);
+	}
+
+	static void MultiValueTypeArray ()
+	{
+		int[,] a = { { 6, 7, 8 }, { 9, 10, 11 } };
+
+		Console.WriteLine (a [1,2]);
+	}
+
+	static void StringArray ()
+	{
+		string[] a = { "Hello", "World" };
+
+		Console.WriteLine (a);
+	}
+
+	static void MultiStringArray ()
+	{
+		string[,] a = { { "Hello", "World" }, { "New York", "Boston" },
+				{ "Ximian", "Monkeys" } };
+
+		Console.WriteLine (a);
+	}
+
+	static void Main ()
+	{
+		Simple ();
+		BoxedValueType ();
+		BoxedReferenceType ();
+		SimpleArray ();
+		MultiValueTypeArray ();
+		StringArray ();
+		MultiStringArray ();
+	}
+}
