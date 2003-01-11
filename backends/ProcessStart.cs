b253efee1443afa2aa64eaef2e564de9b8c26854
Martin Baulig <martin@novell.com>
2003-01-11 17:30:53 +0000

    Pass the correct command line arguments to managed applications.
    svn path=/trunk/debugger/; revision=10382

--- backends/ProcessStart.cs
+++ backends/ProcessStart.cs
@@ -225,7 +225,7 @@ namespace Mono.Debugger
 
 			string[] start_argv = {
 				Path_Mono, "--break", main_name, "--debug=internal_mono_debugger",
-				"--noinline", old_argv [0]
+				"--noinline"
 			};
 
 			string[] new_argv = new string [old_argv.Length + start_argv.Length];
