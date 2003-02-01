0f31065b0763cb4d630e11737ca5d3cc5a866507
Martin Baulig <martin@novell.com>
2003-02-01 18:17:10 +0000

    2003-02-01  Martin Baulig  <martin@ximian.com>
    	* frontends/command-line/Interpreter.cs: The ScriptingContext is
    	now created by the caller and passed to the .ctor as argument.
    
    svn path=/trunk/debugger/; revision=11095

--- frontends/command-line/Interpreter.cs
+++ frontends/command-line/Interpreter.cs
@@ -13,20 +13,13 @@ namespace Mono.Debugger.Frontends.CommandLine
 	/// </summary>
 	public class Interpreter
 	{
-		DebuggerTextWriter command_output, inferior_output;
-		ScriptingContext context;
 		Parser parser;
 
 		// <summary>
 		//   Create a new command interpreter.
-		//   The interpreter sends its stdout to @stdout and its stderr to @stderr.
 		// </summary>
-		public Interpreter (DebuggerTextWriter command_output, DebuggerTextWriter inferior_output)
+		public Interpreter (ScriptingContext context)
 		{
-			this.command_output = command_output;
-			this.inferior_output = inferior_output;
-
-			context = new ScriptingContext (command_output, inferior_output, false, true);
 			parser = new Parser (context, "Debugger");
 		}
 
@@ -40,10 +33,6 @@ namespace Mono.Debugger.Frontends.CommandLine
 			}
 
 			return !parser.Quit;
-		}
-
-		public ScriptingContext Context {
-			get { return context; }
 		}
 	}
 }
