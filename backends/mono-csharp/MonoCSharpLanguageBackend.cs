db91d4abc8a39f309aa07a1d00fc07de07ee5e33
Martin Baulig <martin@novell.com>
2003-02-09 22:47:36 +0000

    Increment the version number.
    svn path=/trunk/debugger/; revision=11425

--- backends/mono-csharp/MonoCSharpLanguageBackend.cs
+++ backends/mono-csharp/MonoCSharpLanguageBackend.cs
@@ -170,7 +170,7 @@ namespace Mono.Debugger.Languages.CSharp
 	// </summary>
 	internal class MonoSymbolFileTable
 	{
-		public const int  DynamicVersion = 22;
+		public const int  DynamicVersion = 23;
 		public const long DynamicMagic   = 0x7aff65af4253d427;
 
 		internal int TotalSize;
