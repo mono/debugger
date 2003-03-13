d27a45e455536d4582a383a210ecefaff2d6dd9b
Martin Baulig <martin@novell.com>
2003-03-13 22:30:27 +0000

    2003-03-13  Martin Baulig  <martin@ximian.com>
    	* test/TestLexicalScopes.cs: New tests.  The debugger should only
    	display one variable called `a' at a time.
    
    svn path=/trunk/debugger/; revision=12486

---
+++ test/TestLexicalScopes.cs
@@ -0,0 +1,31 @@
+using System;
+
+class X
+{
+	static int Main ()
+	{
+		Console.WriteLine ("Begin");
+
+		{
+			int a = 5;
+			Console.WriteLine (a);
+		}
+
+		Console.WriteLine ("Middle");
+
+		{
+			int a = 8;
+			Console.WriteLine (a);
+
+			{
+				long b = 7;
+				Console.WriteLine (b);
+			}
+
+			Console.WriteLine (a);
+		}
+
+		Console.WriteLine ("End");
+		return 0;
+	}
+}
