52f78d0b585842b96ad620e6446c380a161ed811
Martin Baulig <martin@novell.com>
2003-04-07 12:41:02 +0000

    2003-04-07  Martin Baulig  <martin@ximian.com>
    	* configure.in: Require mono and mini for the moment.
    
    	* wrapper/Makefile.am: Build a `mono-debugger-jit-wrapper' for the
    	old JIT and a `mono-debugger-mini-wrapper' for the new one.
    
    	* wrapper/mini-main.c: New file.  This is the main.c for Mini.
    
    	* lib/Makefile.am: Make `mono-debugger-mini-wrapper' the default;
    	we're now using the new JIT in the debugger.
    
    svn path=/trunk/debugger/; revision=13269

---
+++ wrapper/mini-main.c
@@ -0,0 +1,38 @@
+#include <mono-debugger-jit-wrapper.h>
+#include <mono/metadata/verify.h>
+#include <locale.h>
+
+int
+main (int argc, char **argv, char **envp)
+{
+	MonoDomain *domain;
+	const char *file, *error;
+	int retval;
+
+	setlocale(LC_ALL, "");
+	g_log_set_always_fatal (G_LOG_LEVEL_ERROR);
+	g_log_set_fatal_mask (G_LOG_DOMAIN, G_LOG_LEVEL_ERROR);
+
+	g_assert (argc >= 3);
+	file = argv [2];
+
+	g_set_prgname (file);
+
+	mini_parse_default_optimizations (argv [1]);
+
+	domain = mini_init (argv [0]);
+
+	mono_config_parse (NULL);
+
+	error = mono_verify_corlib ();
+	if (error) {
+		fprintf (stderr, "Corlib not in sync with this runtime: %s\n", error);
+		exit (1);
+	}
+
+	retval = mono_debugger_main (domain, file, argc, argv, envp);
+
+	mini_cleanup (domain);
+
+	return retval;
+}
