865e6464c32ef11da04d69189197dc4f0e880314
Martin Baulig <martin@novell.com>
2003-05-16 21:21:15 +0000

    Reflect latest mini changes.
    svn path=/trunk/debugger/; revision=14638

--- wrapper/mini-main.c
+++ wrapper/mini-main.c
@@ -1,14 +1,16 @@
 #include <mono-debugger-jit-wrapper.h>
 #include <mono/metadata/verify.h>
-#include <mono/mini/mini.h>
 #include <locale.h>
+
+extern MonoDomain *
+mono_init_debugger (const char *file, const char *opt_flags);
 
 int
 main (int argc, char **argv, char **envp)
 {
 	MonoDomain *domain;
-	const char *file, *error;
-	int opt, retval;
+	const char *file;
+	int retval;
 
 	setlocale(LC_ALL, "");
 	g_log_set_always_fatal (G_LOG_LEVEL_ERROR);
@@ -17,22 +19,7 @@ main (int argc, char **argv, char **envp)
 	g_assert (argc >= 3);
 	file = argv [2];
 
-	g_set_prgname (file);
-
-	opt = mono_parse_default_optimizations (argv [1]);
-	opt |= MONO_OPT_SHARED;
-
-	mono_set_defaults (0, opt);
-
-	domain = mono_jit_init (argv [0]);
-
-	mono_config_parse (NULL);
-
-	error = mono_verify_corlib ();
-	if (error) {
-		fprintf (stderr, "Corlib not in sync with this runtime: %s\n", error);
-		exit (1);
-	}
+	domain = mono_init_debugger (file, argv [1]);
 
 	retval = mono_debugger_main (domain, file, argc, argv, envp);
 
