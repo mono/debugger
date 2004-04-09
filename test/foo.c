37418587e5c84d9ea9313e3d3045ecbc9cdcdf00
Martin Baulig <martin@novell.com>
2004-04-09 20:55:01 +0000

    *** empty log message ***
    svn path=/trunk/debugger/; revision=25264

--- test/foo.c
+++ test/foo.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 void
 hello (const char *message, int a)
 {
 	printf ("%s - %d\n", message, getpid ());
 	fflush (stdout);
+}
+
+void
+loop (int a)
+{
+	for (;;) {
+		hello ("Hello World", a);
+		sleep (10);
+	}
+}
+
+int
+main (void)
+{
+	int a = 5;
+	loop (a);
+	return 0;
 }
