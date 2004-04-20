b226aed1bc9ea17e6a34caca7662f30e1e48484c
Martin Baulig <martin@novell.com>
2004-04-20 15:34:39 +0000

    Added another test to the test suite.
    svn path=/trunk/debugger/; revision=25729

---
+++ test/TestNamespace.cs
@@ -0,0 +1,24 @@
+namespace Martin
+{
+	namespace Baulig
+	{
+		public class Hello
+		{
+			public static void World ()
+			{ }
+		}
+	}
+}
+
+namespace Test
+{
+	using Martin.Baulig;
+
+	class X
+	{
+		static void Main ()
+		{
+			Hello.World ();
+		}
+	}
+}
