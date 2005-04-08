c9c036da8e6792b8e21f720bbf815dc1c082303a
Martin Baulig <martin@novell.com>
2005-04-08 13:16:07 +0000

    2005-04-08  Martin Baulig  <martin@ximian.com>
    	* backends/server/x86-linux-ptrace.h: Moved `INFERIOR_REGS_TYPE'
    	and the `INFERIOR_REG_*' macros into i386-arch.h.
    
    
    svn path=/trunk/debugger/; revision=42692

--- backends/server/i386-arch.h
+++ backends/server/i386-arch.h
@@ -7,6 +7,29 @@ G_BEGIN_DECLS
 
 #if defined(__i386__)
 
+#include <asm/user.h>
+
+#define INFERIOR_REGS_TYPE	struct user_regs_struct
+#define INFERIOR_FPREGS_TYPE	struct user_i387_struct
+
+#define INFERIOR_REG_EIP(r)	r.eip
+#define INFERIOR_REG_ESP(r)	r.esp
+#define INFERIOR_REG_EBP(r)	r.ebp
+#define INFERIOR_REG_EAX(r)	r.eax
+#define INFERIOR_REG_EBX(r)	r.ebx
+#define INFERIOR_REG_ECX(r)	r.ecx
+#define INFERIOR_REG_EDX(r)	r.edx
+#define INFERIOR_REG_ESI(r)	r.esi
+#define INFERIOR_REG_EDI(r)	r.edi
+#define INFERIOR_REG_EFLAGS(r)	r.eflags
+#define INFERIOR_REG_ESP(r)	r.esp
+#define INFERIOR_REG_FS(r)	r.fs
+#define INFERIOR_REG_ES(r)	r.es
+#define INFERIOR_REG_DS(r)	r.ds
+#define INFERIOR_REG_CS(r)	r.cs
+#define INFERIOR_REG_SS(r)	r.ss
+#define INFERIOR_REG_GS(r)	r.gs
+
 typedef enum {
 	DEBUGGER_REG_EBX = 0,
 	DEBUGGER_REG_ECX,
