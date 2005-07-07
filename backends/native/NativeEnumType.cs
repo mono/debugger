e3eb6c54748b9a19b8da171901d652606a884dee
Chris Toshok <toshok@novell.com>
2005-07-07 21:17:13 +0000

    reindent
    svn path=/trunk/debugger/; revision=47071

--- backends/native/NativeEnumType.cs
+++ backends/native/NativeEnumType.cs
@@ -2,54 +2,54 @@ using System;
 
 namespace Mono.Debugger.Languages.Native
 {
-  internal class NativeEnumType : NativeType, ITargetEnumType
-  {
-  	string[] element_names;
-  	int[] element_values;
+	internal class NativeEnumType : NativeType, ITargetEnumType
+	{
+		string[] element_names;
+		int[] element_values;
 
-  	NativeFieldInfo[] members;
-  	NativeFieldInfo value;
+		NativeFieldInfo[] members;
+		NativeFieldInfo value;
 
-  	public NativeEnumType (string name, int size, string[] element_names, int[] element_values)
-		: base (name, TargetObjectKind.Enum, size, true)
-  	{
-		this.element_names = element_names;
-		this.element_values = element_values;
+		public NativeEnumType (string name, int size, string[] element_names, int[] element_values)
+		  : base (name, TargetObjectKind.Enum, size, true)
+		{
+			this.element_names = element_names;
+			this.element_values = element_values;
 
-		members = new NativeFieldInfo [element_names.Length];
-		int i;
-		for (i = 0; i < element_names.Length; i ++) {
-			members[i] = new NativeFieldInfo (this, element_names[i], i, true, element_values[i]);
+			members = new NativeFieldInfo [element_names.Length];
+			int i;
+			for (i = 0; i < element_names.Length; i ++) {
+				members[i] = new NativeFieldInfo (this, element_names[i], i, true, element_values[i]);
+			}
+		}
+
+
+		public ITargetFieldInfo Value {
+			get {
+				return value;
+			}
+		}
+
+		public ITargetFieldInfo[] Members {
+			get {
+				return members;
+			}
+		}
+
+		public ITargetObject GetMember (StackFrame frame, int index)
+		{
+			return null;
+		}
+
+		public override bool IsByRef {
+			get {
+				return false;
+			}
+		}
+
+		public override NativeObject GetObject (TargetLocation location)
+		{
+			return new NativeEnumObject (this, location);
 		}
 	}
-
-
-  	public ITargetFieldInfo Value {
-		get {
-			return value;
-		}
-	}
-
-  	public ITargetFieldInfo[] Members {
-		get {
-			return members;
-		}
-	}
-
-  	public ITargetObject GetMember (StackFrame frame, int index)
-  	{
-		return null;
-	}
-
-  	public override bool IsByRef {
-		get {
-			return false;
-		}
-	}
-
-  	public override NativeObject GetObject (TargetLocation location)
-  	{
-		return new NativeEnumObject (this, location);
-	}
-  }
 }
