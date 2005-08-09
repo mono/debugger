605262a8e73502af953a93335d7b81fa0d467449
Martin Baulig <martin@novell.com>
2005-08-09 19:56:12 +0000

    Updated.
    svn path=/trunk/debugger/; revision=48197

---
+++ backends/remoting/DebuggerClient.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Activation;
+using System.Runtime.Remoting.Channels;
+
+namespace Mono.Debugger.Remoting
+{
+	public static class DebuggerClient
+	{
+		static DebuggerClientChannel channel;
+
+		static DebuggerClient ()
+		{
+			channel = new DebuggerClientChannel ();
+			ChannelServices.RegisterChannel (channel);
+		}
+
+		public static DebuggerBackend CreateConnection ()
+		{
+			string base_directory = System.AppDomain.CurrentDomain.BaseDirectory;
+
+			/* Use relative path based on where Mono.Debugger.Remoting.dll is at to enable relocation */
+			string path = Path.GetFullPath (
+				base_directory + Path.DirectorySeparatorChar + "mdb-server.exe");
+			string url = "mdb://" + Environment.MachineName + ":" + path + "!DebuggerBackend";
+
+			DebuggerBackend backend;
+#if FIXME
+			object[] url_arg = new object[] { new UrlAttribute (url) };
+			backend = (DebuggerBackend) Activator.CreateInstance (
+				typeof (DebuggerBackend), null, url_arg);
+#else
+			backend = (DebuggerBackend) Activator.GetObject (typeof (DebuggerBackend), url);
+#endif
+
+			return backend;
+		}
+
+		public static void Shutdown ()
+		{
+			ChannelServices.UnregisterChannel (channel);
+			channel.Dispose ();
+			channel = null;
+		}
+	}
+}
