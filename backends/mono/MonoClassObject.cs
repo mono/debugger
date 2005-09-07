e9f5204511761c3e559808198bad0ba1a600a4c8
Martin Baulig <martin@novell.com>
2005-09-07 17:20:59 +0000

    2005-09-07  Martin Baulig  <martin@ximian.com>
    	* interfaces/ITargetStructType.cs
    	(ITargetStructType.PrintObject): Removed.
    	
    
    svn path=/trunk/debugger/; revision=49647

--- backends/mono/MonoClassObject.cs
+++ backends/mono/MonoClassObject.cs
@@ -52,42 +52,6 @@ namespace Mono.Debugger.Languages.Mono
 			return type.Type.GetMethod (location.TargetAccess, index);
 		}
 
-		public string PrintObject ()
-		{
-			ITargetObject[] args = new ITargetObject[0];
-			ITargetFunctionObject func = null;
-			ITargetStructType stype = (ITargetStructType)type.Type;
-
-		again:
-			foreach (ITargetMethodInfo m in stype.Methods)
-				// XXX also check that the return type is String, right?
-				if (m.Name == "ToString" && m.Type.ParameterTypes.Length == 0) {
-					func = GetMethod (m.Index);
-					break;
-				}
-
-			if (func == null) {
-				ITargetClassType ctype = stype as ITargetClassType;
-				if (ctype != null && ctype.HasParent) {
-					stype = ctype.ParentType;
-					goto again;
-				}
-				else {
-					return null;
-				}
-			}
-
-			try {
-				ITargetObject retval = func.Invoke (
-					location.TargetAccess, this, args);
-
-				return retval.Print();
-			}
-			catch (TargetInvocationException ex) {
-				return "Exception calling ToString()";
-			}
-		}
-
 		protected override long GetDynamicSize (TargetBlob blob, TargetLocation location,
 							out TargetLocation dynamic_location)
 		{
--- interfaces/ITargetStructObject.cs
+++ interfaces/ITargetStructObject.cs
@@ -13,11 +13,5 @@ namespace Mono.Debugger.Languages
 		ITargetObject GetEvent (int index);
 
 		ITargetFunctionObject GetMethod (int index);
-
-		// <summary>
-		//   Calls a function in the target to get a textual representation
-		//   of the object.  For CIL applications, this'll call Object.ToString().
-		// </summary>
-		string PrintObject ();
 	}
 }
