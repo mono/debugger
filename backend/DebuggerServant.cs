c848a3360f7cdba41363a4976a050006e0e8aaba
Martin Baulig <martin@novell.com>
2007-01-29 14:55:48 +0000

    2007-01-29  Martin Baulig  <martin@ximian.com>
    	* backend/server/x86-ptrace.c
    	(server_ptrace_spawn): Use mono_domain_get() instead of
    	mono_get_root_domain() to execute the callback in the correct domain.
    
    
    svn path=/trunk/debugger/; revision=71874

--- backend/DebuggerServant.cs
+++ backend/DebuggerServant.cs
@@ -29,7 +29,7 @@ namespace Mono.Debugger.Backends
 		{
 			this.client = client;
 			this.config = config;
-			Report.ReportWriter = writer;
+			Report.Initialize (writer);
 			ObjectCache.Initialize ();
 			thread_manager = new ThreadManager (this);
 			process_hash = Hashtable.Synchronized (new Hashtable ());
