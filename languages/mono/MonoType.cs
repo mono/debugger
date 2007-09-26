b8569b17da79c35f3330a605c10c4e1220deadb6
Martin Baulig <martin@novell.com>
2007-09-26 15:39:50 +0000

    Ooops, ok - this didn't work.
    svn path=/trunk/debugger/; revision=86412

--- languages/mono/MonoType.cs
+++ languages/mono/MonoType.cs
@@ -115,7 +115,7 @@ namespace Mono.Debugger.Languages.Mono
 
 			case MonoTypeEnum.MONO_TYPE_VALUETYPE:
 			case MonoTypeEnum.MONO_TYPE_CLASS:
-				return mono.ReadClassInfo (memory, data).ClassType;
+				return mono.ReadMonoClass (memory, data);
 
 			case MonoTypeEnum.MONO_TYPE_SZARRAY: {
 				TargetType etype = mono.ReadMonoClass (memory, data);
