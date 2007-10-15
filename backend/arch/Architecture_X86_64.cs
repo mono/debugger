f1363633a136c875b8f81a5fde741a861cb103c7
Martin Baulig <martin@novell.com>
2007-10-15 12:55:29 +0000

    2007-10-15  Martin Baulig  <martin@ximian.com>
    	* backend/arch/Architecture_X86_64.cs
    	(Architecture_X86_64.GetTrampoline): Add support for the new
    	common vtable trampolines.
    
    
    svn path=/trunk/debugger/; revision=87505

--- backend/arch/Architecture_X86_64.cs
+++ backend/arch/Architecture_X86_64.cs
@@ -222,26 +222,23 @@ namespace Mono.Debugger.Backends
 			if (trampoline_address.IsNull)
 				return TargetAddress.Null;
 
-			TargetBinaryReader reader = target.ReadMemory (location, 19).GetReader ();
-
-			reader.Position = 9;
+			TargetBinaryReader reader = target.ReadMemory (location, 14).GetReader ();
 
 			byte opcode = reader.ReadByte ();
-			if (opcode != 0x68)
+			if (opcode != 0xe8)
 				return TargetAddress.Null;
 
-			int method_info = reader.ReadInt32 ();
-
-			opcode = reader.ReadByte ();
-			if (opcode != 0xe9)
+			TargetAddress call_target = location + reader.ReadInt32 () + 5;
+			if (call_target != trampoline_address)
 				return TargetAddress.Null;
 
-			int call_disp = reader.ReadInt32 ();
+			long method;
+			if (reader.ReadByte () == 0x04)
+				method = reader.ReadInt32 ();
+			else
+				method = reader.ReadInt64 ();
 
-			if (location + call_disp + 19 != trampoline_address)
-				return TargetAddress.Null;
-
-			return new TargetAddress (AddressDomain, method_info);
+			return new TargetAddress (target.TargetInfo.AddressDomain, method);
 		}
 
 		public override string[] RegisterNames {
