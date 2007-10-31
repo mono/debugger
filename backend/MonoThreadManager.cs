a4762fa0269a45dc16ce8ca03584ca90bb244c60
Martin Baulig <martin@novell.com>
2007-10-31 18:09:00 +0000

    2007-10-31  Martin Baulig  <martin@ximian.com>
    	HEADS UP: The debugger only works on x86_64 at the moment.
    
    	You need to use the runtime from the plophos branch for the moment.
    
    	* backend/MonoThreadManager.cs
    	(MonoDebuggerInfo.MinDynamicVersion): Put back to 63 to require Plophos.
    
    
    svn path=/trunk/debugger/; revision=88594

--- backend/MonoThreadManager.cs
+++ backend/MonoThreadManager.cs
@@ -311,8 +311,8 @@ namespace Mono.Debugger.Backends
 	internal class MonoDebuggerInfo
 	{
 		// These constants must match up with those in mono/mono/metadata/mono-debug.h
-		public const int  MinDynamicVersion = 64;
-		public const int  MaxDynamicVersion = 64;
+		public const int  MinDynamicVersion = 63;
+		public const int  MaxDynamicVersion = 63;
 		public const long DynamicMagic      = 0x7aff65af4253d427;
 
 		public readonly TargetAddress NotificationAddress;
