5ed153c6373cc1dbae5160cc067581eeaeac7858
Martin Baulig <martin@novell.com>
2007-11-27 17:55:52 +0000

    svn path=/branches/martin/debugger-madrid/debugger/; revision=90359

--- languages/mono/OldMonoRuntime.cs
+++ languages/mono/OldMonoRuntime.cs
@@ -4,71 +4,22 @@ using Mono.Debugger.Backends;
 
 namespace Mono.Debugger.Languages.Mono
 {
-	internal enum MonoTypeEnum
-	{
-		MONO_TYPE_END        = 0x00,       /* End of List */
-		MONO_TYPE_VOID       = 0x01,
-		MONO_TYPE_BOOLEAN    = 0x02,
-		MONO_TYPE_CHAR       = 0x03,
-		MONO_TYPE_I1         = 0x04,
-		MONO_TYPE_U1         = 0x05,
-		MONO_TYPE_I2         = 0x06,
-		MONO_TYPE_U2         = 0x07,
-		MONO_TYPE_I4         = 0x08,
-		MONO_TYPE_U4         = 0x09,
-		MONO_TYPE_I8         = 0x0a,
-		MONO_TYPE_U8         = 0x0b,
-		MONO_TYPE_R4         = 0x0c,
-		MONO_TYPE_R8         = 0x0d,
-		MONO_TYPE_STRING     = 0x0e,
-		MONO_TYPE_PTR        = 0x0f,       /* arg: <type> token */
-		MONO_TYPE_BYREF      = 0x10,       /* arg: <type> token */
-		MONO_TYPE_VALUETYPE  = 0x11,       /* arg: <type> token */
-		MONO_TYPE_CLASS      = 0x12,       /* arg: <type> token */
-		MONO_TYPE_VAR	     = 0x13,	   /* number */
-		MONO_TYPE_ARRAY      = 0x14,       /* type, rank, boundsCount, bound1, loCount, lo1 */
-		MONO_TYPE_GENERICINST= 0x15,	   /* <type> <type-arg-count> <type-1> \x{2026} <type-n> */
-		MONO_TYPE_TYPEDBYREF = 0x16,
-		MONO_TYPE_I          = 0x18,
-		MONO_TYPE_U          = 0x19,
-		MONO_TYPE_FNPTR      = 0x1b,	      /* arg: full method signature */
-		MONO_TYPE_OBJECT     = 0x1c,
-		MONO_TYPE_SZARRAY    = 0x1d,       /* 0-based one-dim-array */
-		MONO_TYPE_MVAR	     = 0x1e,       /* number */
-		MONO_TYPE_CMOD_REQD  = 0x1f,       /* arg: typedef or typeref token */
-		MONO_TYPE_CMOD_OPT   = 0x20,       /* optional arg: typedef or typref token */
-		MONO_TYPE_INTERNAL   = 0x21,       /* CLR internal type */
-
-		MONO_TYPE_MODIFIER   = 0x40,       /* Or with the following types */
-		MONO_TYPE_SENTINEL   = 0x41,       /* Sentinel for varargs method signature */
-		MONO_TYPE_PINNED     = 0x45,       /* Local var that points to pinned object */
-
-		MONO_TYPE_ENUM       = 0x55        /* an enumeration */
-	}
-
 	internal static class OldMonoRuntime
 	{
 		public static TargetType ReadMonoClass (MonoLanguageBackend mono,
 							TargetMemoryAccess target,
-							TargetAddress address)
+							TargetAddress klass)
 		{
-			int byval_offset = mono.MonoMetadataInfo.KlassByValArgOffset;
-			return ReadType (mono, target, address + byval_offset);
+			TargetAddress byval_type = mono.MonoRuntime.MonoClassGetByValType (target, klass);
+			return ReadType (mono, target, byval_type);
 		}
 
 		public static TargetType ReadType (MonoLanguageBackend mono, TargetMemoryAccess memory,
 						   TargetAddress address)
 		{
-			TargetAddress data = memory.ReadAddress (address);
-			uint flags = (uint) memory.ReadInteger (
-				address + memory.TargetMemoryInfo.TargetAddressSize);
-
-			MonoTypeEnum type = (MonoTypeEnum) ((flags & 0x00ff0000) >> 16);
-			bool byref = (int) ((flags & 0x40000000) >> 30) != 0;
-
-			// int attrs = (int) (flags & 0x0000ffff);
-			// int num_mods = (int) ((flags & 0x3f000000) >> 24);
-			// int pinned = (int) ((flags & 0x80000000) >> 31);
+			TargetAddress data = mono.MonoRuntime.MonoTypeGetData (memory, address);
+			MonoTypeEnum type = mono.MonoRuntime.MonoTypeGetType (memory, address);
+			bool byref = mono.MonoRuntime.MonoTypeGetIsByRef (memory, address);
 
 			TargetType target_type = ReadType (mono, memory, type, data);
 			if (target_type == null)
@@ -156,11 +107,9 @@ namespace Mono.Debugger.Languages.Mono
 							       TargetMemoryAccess memory,
 							       TargetAddress address)
 		{
-			MonoMetadataInfo info = mono.MonoMetadataInfo;
-
-			int token = memory.ReadInteger (address + info.MonoMethodTokenOffset);
-			TargetAddress klass = memory.ReadAddress (address + info.MonoMethodKlassOffset);
-			TargetAddress image = memory.ReadAddress (klass + info.KlassImageOffset);
+			int token = mono.MonoRuntime.MonoMethodGetToken (memory, address);
+			TargetAddress klass = mono.MonoRuntime.MonoMethodGetClass (memory, address);
+			TargetAddress image = mono.MonoRuntime.MonoClassGetMonoImage (memory, klass);
 
 			MonoSymbolFile file = mono.GetImage (image);
 			if (file == null)
