42324a29d0b336bebf0f501caec83e63d4e76911
Martin Baulig <martin@novell.com>
2008-08-13 22:46:55 +0000

    2008-08-14  Martin Baulig  <martin@ximian.com>
    	* test/{src|testsuite}/TestActivateBreakpoints2.cs: New test.
    
    
    svn path=/trunk/debugger/; revision=110422

---
+++ test/src/TestActivateBreakpoints2.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Threading;
+
+class X
+{
+	long count;
+	bool loop;
+	Thread blocking, sleeping, executing;
+	ManualResetEvent blocking_event = new ManualResetEvent (false);
+
+	X (bool loop)
+	{
+		this.loop = loop;
+	}
+
+	void BlockingThread ()
+	{
+		blocking_event.WaitOne ();
+		Console.WriteLine ("Blocking Done");
+	}
+
+	void SleepingThread ()
+	{
+		while (loop) {
+			Thread.Sleep (100);
+		}
+	}
+
+	void ExecutingThread ()
+	{
+		while (loop) {
+			count++;		// @MDB LINE: executing
+		}
+		blocking_event.Set ();
+	}
+
+	void StartThreads ()
+	{
+		blocking = new Thread (new ThreadStart (BlockingThread));
+		blocking.Start ();
+
+		sleeping = new Thread (new ThreadStart (SleepingThread));	// @MDB BREAKPOINT: thread start1
+		sleeping.Start ();
+
+		executing = new Thread (new ThreadStart (ExecutingThread));	// @MDB BREAKPOINT: thread start2
+		executing.Start ();
+
+		executing.Join ();						// @MDB BREAKPOINT: thread start3
+	}
+
+	static void Main (params string[] args)
+	{
+		X x = new X (args.Length == 0);					// @MDB LINE: main
+		x.StartThreads ();
+	}
+}
---
+++ test/testsuite/TestActivateBreakpoints2.cs
@@ -0,0 +1,85 @@
+using System;
+using NUnit.Framework;
+
+using Mono.Debugger;
+using Mono.Debugger.Languages;
+using Mono.Debugger.Frontend;
+
+namespace Mono.Debugger.Tests
+{
+	[TestFixture]
+	public class TestActivateBreakpoints2 : TestSuite
+	{
+		public TestActivateBreakpoints2 ()
+			: base ("TestActivateBreakpoints2")
+		{
+			Config.BrokenThreading = false;
+			Config.StayInThread = true;
+		}
+
+		[Test]
+		[Category("GUI")]
+		public void Main ()
+		{
+			Process process = Start ();
+			Assert.IsTrue (process.IsManaged);
+			Assert.IsTrue (process.MainThread.IsStopped);
+
+			Thread thread = process.MainThread;
+
+			AssertStopped (thread, "main", "X.Main(string[])");
+
+			AssertExecute ("continue");
+			Thread blocking = AssertThreadCreated ();
+			AssertHitBreakpoint (thread, "thread start1", "X.StartThreads()");
+			AssertExecute ("continue");
+			Thread sleeping = AssertThreadCreated ();
+			AssertHitBreakpoint (thread, "thread start2", "X.StartThreads()");
+			AssertExecute ("continue");
+			Thread executing = AssertThreadCreated ();
+			AssertHitBreakpoint (thread, "thread start3", "X.StartThreads()");
+
+			AssertExecute ("bg");
+			int bpt_executing = (int) AssertExecute ("break -gui -global " + GetLine ("executing"));
+
+			AssertHitBreakpoint (executing, bpt_executing, "X.ExecutingThread()", GetLine ("executing"));
+			Interpreter.CurrentThread = executing;
+			AssertExecute ("set loop = false");
+			AssertPrint (executing, "loop", "(bool) false");
+			AssertExecute ("bg");
+
+			AssertTargetOutput ("Blocking Done");
+
+			int status = 0;
+			while (status != 15) {
+				DebuggerEvent e = AssertEvent ();
+				if (e.Type == DebuggerEventType.ThreadExited) {
+					if (e.Data == blocking) {
+						status |= 1;
+						continue;
+					} else if (e.Data == executing) {
+						status |= 2;
+						continue;
+					}
+				} else if (e.Type == DebuggerEventType.TargetEvent) {
+					Thread e_thread = (Thread) e.Data;
+					TargetEventArgs args = (TargetEventArgs) e.Data2;
+
+					if (args.Type == TargetEventType.TargetExited) {
+						if (e_thread == blocking) {
+							status |= 4;
+							continue;
+						} else if (e_thread == executing) {
+							status |= 8;
+							continue;
+						}
+					}
+				}
+
+				Assert.Fail ("Received unexpected event {0}.", e);
+			}
+
+			AssertTargetExited (process);
+		}
+	}
+}
