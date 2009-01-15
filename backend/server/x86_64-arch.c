mono/debugger

commit 6023223a6cacac05a5eefaca58fe3c568a466b2d

2008-12-08  Martin Baulig  <martin@ximian.com>
	* backend/server/i386-arch.c (x86_arch_child_stopped): Send
	`STOP_ACTION_STOPPED' without checking for breakpoint if we didn't
	receive a SIGTRAP.

	* backend/server/x86_64-arch.c (x86_arch_child_stopped): Likewise.


svn path=/trunk/debugger/; revision=120992

backend/server/x86_64-arch.c
@@ -186,6 +186,11 @@ x86_arch_child_stopped (ServerHandle *handle, int stopsig,
 	if (stopsig == SIGSTOP)
 		return STOP_ACTION_INTERRUPTED;
 
+#if defined(__linux__) || defined(__FreeBSD__)
+	if (stopsig != SIGTRAP)
+		return STOP_ACTION_STOPPED;
+#endif
+
 	if (handle->mono_runtime &&
 	    (INFERIOR_REG_RIP (arch->current_regs) - 1 == handle->mono_runtime->notification_address)) {
 		if (stopsig != SIGTRAP)
@@ -307,11 +312,6 @@ x86_arch_child_stopped (ServerHandle *handle, int stopsig,
 		arch->code_buffer = NULL;
 		return STOP_ACTION_STOPPED;
 	}
-
-#if defined(__linux__) || defined(__FreeBSD__)
-	if (stopsig != SIGTRAP)
-		return STOP_ACTION_STOPPED;
-#endif
 
 	if (server_ptrace_peek_word (handle, GPOINTER_TO_SIZE(INFERIOR_REG_RIP (arch->current_regs) - 1), &code) != COMMAND_ERROR_NONE)
 		return STOP_ACTION_STOPPED;

backend/server/i386-arch.c
@@ -507,6 +507,11 @@ x86_arch_child_stopped (ServerHandle *handle, int stopsig,
 	if (stopsig == SIGSTOP)
 		return STOP_ACTION_INTERRUPTED;
 
+#if defined(__linux__) || defined(__FreeBSD__)
+	if (stopsig != SIGTRAP)
+		return STOP_ACTION_STOPPED;
+#endif
+
 	if (handle->mono_runtime &&
 	    ((guint32) INFERIOR_REG_EIP (arch->current_regs) - 1 == handle->mono_runtime->notification_address)) {
 		guint32 addr = (guint32) INFERIOR_REG_ESP (arch->current_regs) + 4;
@@ -627,11 +632,6 @@ x86_arch_child_stopped (ServerHandle *handle, int stopsig,
 		arch->code_buffer = NULL;
 		return STOP_ACTION_STOPPED;
 	}
-
-#if defined(__linux__) || defined(__FreeBSD__)
-	if (stopsig != SIGTRAP)
-		return STOP_ACTION_STOPPED;
-#endif
 
 	if (server_ptrace_peek_word (handle, GPOINTER_TO_SIZE (INFERIOR_REG_EIP (arch->current_regs) - 1), &code) != COMMAND_ERROR_NONE)
 		return STOP_ACTION_STOPPED;

commit fd93e5f768bb5fbac68b30ace90265c1b42d22a3

2009-01-15  Martin Baulig  <martin@ximian.com>
	* backend/server/x86_64-arch.c
	(x86_arch_child_stopped): Fix the regression from r120992 (Dec 8th):
	Check for callbacks first because we expect to receive a SIGSEGV
	on the non-executable stack.


svn path=/trunk/debugger/; revision=123467

backend/server/x86_64-arch.c
@@ -186,38 +186,6 @@ x86_arch_child_stopped (ServerHandle *handle, int stopsig,
 	if (stopsig == SIGSTOP)
 		return STOP_ACTION_INTERRUPTED;
 
-#if defined(__linux__) || defined(__FreeBSD__)
-	if (stopsig != SIGTRAP)
-		return STOP_ACTION_STOPPED;
-#endif
-
-	if (handle->mono_runtime &&
-	    (INFERIOR_REG_RIP (arch->current_regs) - 1 == handle->mono_runtime->notification_address)) {
-		if (stopsig != SIGTRAP)
-			return STOP_ACTION_STOPPED;
-
-		*callback_arg = INFERIOR_REG_RDI (arch->current_regs);
-		*retval = INFERIOR_REG_RSI (arch->current_regs);
-		*retval2 = INFERIOR_REG_RDX (arch->current_regs);
-
-		return STOP_ACTION_NOTIFICATION;
-	}
-
-	for (i = 0; i < DR_NADDR; i++) {
-		if (X86_DR_WATCH_HIT (arch, i)) {
-			_server_ptrace_set_dr (inferior, DR_STATUS, 0);
-			arch->dr_status = 0;
-			*retval = arch->dr_regs [i];
-			return STOP_ACTION_BREAKPOINT_HIT;
-		}
-	}
-
-	if (check_breakpoint (handle, INFERIOR_REG_RIP (arch->current_regs) - 1, retval)) {
-		INFERIOR_REG_RIP (arch->current_regs)--;
-		_server_ptrace_set_registers (inferior, &arch->current_regs);
-		return STOP_ACTION_BREAKPOINT_HIT;
-	}
-
 	cdata = get_callback_data (arch);
 	if (cdata && (cdata->call_address == INFERIOR_REG_RIP (arch->current_regs))) {
 		if (cdata->pushed_registers) {
@@ -279,6 +247,38 @@ x86_arch_child_stopped (ServerHandle *handle, int stopsig,
 
 		g_free (cdata);
 		return STOP_ACTION_CALLBACK;
+	}
+
+#if defined(__linux__) || defined(__FreeBSD__)
+	if (stopsig != SIGTRAP)
+		return STOP_ACTION_STOPPED;
+#endif
+
+	if (handle->mono_runtime &&
+	    (INFERIOR_REG_RIP (arch->current_regs) - 1 == handle->mono_runtime->notification_address)) {
+		if (stopsig != SIGTRAP)
+			return STOP_ACTION_STOPPED;
+
+		*callback_arg = INFERIOR_REG_RDI (arch->current_regs);
+		*retval = INFERIOR_REG_RSI (arch->current_regs);
+		*retval2 = INFERIOR_REG_RDX (arch->current_regs);
+
+		return STOP_ACTION_NOTIFICATION;
+	}
+
+	for (i = 0; i < DR_NADDR; i++) {
+		if (X86_DR_WATCH_HIT (arch, i)) {
+			_server_ptrace_set_dr (inferior, DR_STATUS, 0);
+			arch->dr_status = 0;
+			*retval = arch->dr_regs [i];
+			return STOP_ACTION_BREAKPOINT_HIT;
+		}
+	}
+
+	if (check_breakpoint (handle, INFERIOR_REG_RIP (arch->current_regs) - 1, retval)) {
+		INFERIOR_REG_RIP (arch->current_regs)--;
+		_server_ptrace_set_registers (inferior, &arch->current_regs);
+		return STOP_ACTION_BREAKPOINT_HIT;
 	}
 
 	cbuffer = arch->code_buffer;