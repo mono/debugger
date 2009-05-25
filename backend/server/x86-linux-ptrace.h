cbe64854fd6472120a2a17ad6b05dd7fd64c05fa
Martin Baulig <martin@novell.com>
2009-05-25 17:55:41 +0000

    Moved function prototypes from x86-linux-ptrace.h to x86-ptrace.h.
    svn path=/trunk/debugger/; revision=134703

--- backend/server/x86-linux-ptrace.h
+++ backend/server/x86-linux-ptrace.h
@@ -8,52 +8,6 @@ struct OSData
 	int mem_fd;
 };
 
-static ServerCommandError
-_server_ptrace_check_errno (InferiorHandle *);
-
-static ServerCommandError
-_server_ptrace_get_registers (InferiorHandle *inferior, INFERIOR_REGS_TYPE *regs);
-
-static ServerCommandError
-_server_ptrace_set_registers (InferiorHandle *inferior, INFERIOR_REGS_TYPE *regs);
-
-static ServerCommandError
-_server_ptrace_get_fp_registers (InferiorHandle *inferior, INFERIOR_FPREGS_TYPE *regs);
-
-static ServerCommandError
-_server_ptrace_set_fp_registers (InferiorHandle *inferior, INFERIOR_FPREGS_TYPE *regs);
-
-static ServerCommandError
-_server_ptrace_read_memory (ServerHandle *handle, guint64 start,
-			    guint32 size, gpointer buffer);
-
-static ServerCommandError
-server_ptrace_read_memory (ServerHandle *handle, guint64 start, guint32 size, gpointer buffer);
-
-static ServerCommandError
-_server_ptrace_set_dr (InferiorHandle *handle, int regnum, guint64 value);
-
-static ServerCommandError
-_server_ptrace_get_dr (InferiorHandle *handle, int regnum, guint64 *value);
-
-static ServerCommandError
-server_ptrace_stop (ServerHandle *handle);
-
-static ServerCommandError
-server_ptrace_stop_and_wait (ServerHandle *handle, guint32 *status);
-
-static ServerCommandError
-_server_ptrace_setup_inferior (ServerHandle *handle);
-
-static void
-_server_ptrace_finalize_inferior (ServerHandle *handle);
-
-static ServerCommandError
-server_ptrace_get_signal_info (ServerHandle *handle, SignalInfo **sinfo);
-
-static gboolean
-_server_ptrace_wait_for_new_thread (ServerHandle *handle);
-
 #include "x86-ptrace.h"
 
 #endif
