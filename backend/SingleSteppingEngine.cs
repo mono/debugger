mono/debugger

commit cffcc8a082525adb055069bd83bfd6f077711e17

Fix the 'operation_completed_event' mess. 2009-06-08  Martin Baulig  <martin@ximian.com>
	* backend/ThreadServant.cs
	(ThreadServant.WaitHandle): New public abstract property.

	* classes/Thread.cs
	(Thread.WaitHandle): Use `servant.WaitHandle'.
	(Thread.operation_completed_event): Removed; use the SSE's
	wait handle instead.
	(CommandResult.Completed): Make this internal, not public.
	(OperationCommandResult): New public class; derives from
	`ThreadCommandResult', but has its own wait handle.  This is set
	when the operation is completed, which is not necessarily when the
	target is stopped.
	(RuntimeInvokeResult): Derive from `OperationCommandResult'.

svn path=/trunk/debugger/; revision=135640

backend/SingleSteppingEngine.cs
@@ -528,6 +528,7 @@ namespace Mono.Debugger.Backend
 				remove_temporary_breakpoint ();
 				engine_stopped = true;
 				last_target_event = result;
+				operation_completed_event.Set ();
 				Report.Debug (DebugFlags.EventLoop, "{0} {1} operation {2}: {3}",
 					      this, suspended ? "suspending" : "terminating", current_operation, result);
 
@@ -542,8 +543,8 @@ namespace Mono.Debugger.Backend
 					if (result != null)
 						process.OnTargetEvent (this, result);
 					if (current_operation != null) {
-						Report.Debug (DebugFlags.EventLoop, "{0} setting completed: {1}",
-							      this, current_operation.Result);
+						Report.Debug (DebugFlags.EventLoop, "{0} setting completed: {1} {2}",
+							      this, current_operation, current_operation.Result);
 						current_operation.CompletedOperation ();
 						current_operation = null;
 					}
@@ -774,6 +775,10 @@ namespace Mono.Debugger.Backend
 
 		public override bool IsStopped {
 			get { return engine_stopped; }
+		}
+
+		public override WaitHandle WaitHandle {
+			get { return operation_completed_event; }
 		}
 
 		internal override ProcessServant ProcessServant {
@@ -2335,6 +2340,8 @@ namespace Mono.Debugger.Backend
 		Stack<InterruptibleOperation> nested_break_stack = new Stack<InterruptibleOperation> ();
 		Stack<OperationRuntimeInvoke> rti_stack = new Stack<OperationRuntimeInvoke> ();
 
+		ManualResetEvent operation_completed_event = new ManualResetEvent (false);
+
 #region Nested SSE classes
 		protected sealed class StackData : DebuggerMarshalByRefObject
 		{
@@ -2684,7 +2691,7 @@ namespace Mono.Debugger.Backend
 				operation.Abort ();
 			}
 
-			public override void Completed ()
+			internal override void Completed ()
 			{
 				completed_event.Set ();
 			}

backend/ThreadServant.cs
@@ -272,6 +272,10 @@ namespace Mono.Debugger.Backend
 			get;
 		}
 
+		public abstract ST.WaitHandle WaitHandle {
+			get;
+		}
+
 		public abstract TargetMemoryInfo TargetMemoryInfo {
 			get;
 		}

backend/arch/CoreFile.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Collections;
+using ST = System.Threading;
 using System.Runtime.InteropServices;
 using Mono.Debugger.Languages;
 using Mono.Debugger.Languages.Mono;
@@ -306,6 +307,10 @@ namespace Mono.Debugger.Backend
 
 			public override bool IsStopped {
 				get { return true; }
+			}
+
+			public override ST.WaitHandle WaitHandle {
+				get { throw new InvalidOperationException (); }
 			}
 
 			public override Backtrace GetBacktrace (Backtrace.Mode mode, int max_frames)

classes/Process.cs
@@ -152,7 +152,7 @@ namespace Mono.Debugger
 				get { return completed_event; }
 			}
 
-			public override void Completed ()
+			internal override void Completed ()
 			{
 				completed_event.Set ();
 			}

classes/Thread.cs
@@ -33,18 +33,14 @@ namespace Mono.Debugger
 		{
 			this.id = id;
 			this.servant = servant;
-			this.is_running = true;
-			this.operation_completed_event = new ST.ManualResetEvent (false);
 		}
 
 		int id;
 		Flags flags;
-		bool is_running;
-		ST.ManualResetEvent operation_completed_event;
 		ThreadServant servant;
 
 		public ST.WaitHandle WaitHandle {
-			get { return operation_completed_event; }
+			get { return servant.WaitHandle; }
 		}
 
 		protected internal Language NativeLanguage {
@@ -85,7 +81,6 @@ namespace Mono.Debugger
 			lock (this) {
 				TargetEventArgs args = servant.LastTargetEvent;
 				if (args != null) {
-					is_running = false;
 					flags &= ~Flags.Background;
 					return args;
 				}
@@ -94,7 +89,7 @@ namespace Mono.Debugger
 		}
 
 		public bool IsRunning {
-			get { return is_running; }
+			get { return (servant != null) && !servant.IsStopped; }
 		}
 
 		public bool IsAlive {
@@ -295,11 +290,6 @@ namespace Mono.Debugger
 			}
 		}
 
-		internal void SetCompleted ()
-		{
-			operation_completed_event.Set ();
-		}
-
 		// <summary>
 		//   Step one machine instruction, but don't step into trampolines.
 		// </summary>
@@ -307,9 +297,7 @@ namespace Mono.Debugger
 		{
 			lock (this) {
 				check_alive ();
-				is_running = true;
-				operation_completed_event.Reset ();
-				ThreadCommandResult result = new ThreadCommandResult (this);
+				ThreadCommandResult result = new OperationCommandResult (this);
 				servant.StepInstruction (result);
 				return result;
 			}
@@ -322,9 +310,7 @@ namespace Mono.Debugger
 		{
 			lock (this) {
 				check_alive ();
-				is_running = true;
-				operation_completed_event.Reset ();
-				ThreadCommandResult result = new ThreadCommandResult (this);
+				ThreadCommandResult result = new OperationCommandResult (this);
 				servant.StepNativeInstruction (result);
 				return result;
 			}
@@ -337,9 +323,7 @@ namespace Mono.Debugger
 		{
 			lock (this) {
 				check_alive ();
-				is_running = true;
-				operation_completed_event.Reset ();
-				ThreadCommandResult result = new ThreadCommandResult (this);
+				ThreadCommandResult result = new OperationCommandResult (this);
 				servant.NextInstruction (result);
 				return result;
 			}
@@ -352,9 +336,7 @@ namespace Mono.Debugger
 		{
 			lock (this) {
 				check_alive ();
-				is_running = true;
-				operation_completed_event.Reset ();
-				ThreadCommandResult result = new ThreadCommandResult (this);
+				ThreadCommandResult result = new OperationCommandResult (this);
 				servant.StepLine (result);
 				return result;
 			}
@@ -367,9 +349,7 @@ namespace Mono.Debugger
 		{
 			lock (this) {
 				check_alive ();
-				is_running = true;
-				operation_completed_event.Reset ();
-				ThreadCommandResult result = new ThreadCommandResult (this);
+				ThreadCommandResult result = new OperationCommandResult (this);
 				servant.NextLine (result);
 				return result;
 			}
@@ -382,9 +362,7 @@ namespace Mono.Debugger
 		{
 			lock (this) {
 				check_alive ();
-				is_running = true;
-				operation_completed_event.Reset ();
-				ThreadCommandResult result = new ThreadCommandResult (this);
+				ThreadCommandResult result = new OperationCommandResult (this);
 				servant.Finish (native, result);
 				return result;
 			}
@@ -399,10 +377,8 @@ namespace Mono.Debugger
 		{
 			lock (this) {
 				check_alive ();
-				is_running = true;
-				operation_completed_event.Reset ();
-				ThreadCommandResult result = new ThreadCommandResult (this);
-				servant.Continue (until, new ThreadCommandResult (this));
+				ThreadCommandResult result = new OperationCommandResult (this);
+				servant.Continue (until, result);
 				return result;
 			}
 		}
@@ -416,18 +392,15 @@ namespace Mono.Debugger
 		{
 			lock (this) {
 				check_alive ();
-				is_running = true;
 				flags |= Flags.Background;
-				operation_completed_event.Reset ();
-				ThreadCommandResult result = new ThreadCommandResult (this);
-				servant.Background (until, new ThreadCommandResult (this));
+				ThreadCommandResult result = new OperationCommandResult (this);
+				servant.Background (until, result);
 				return result;
 			}
 		}
 
 		internal void Kill ()
 		{
-			operation_completed_event.Set ();
 			if (servant != null)
 				servant.Kill ();
 			Dispose ();
@@ -435,7 +408,6 @@ namespace Mono.Debugger
 
 		internal void Detach ()
 		{
-			operation_completed_event.Set ();
 			if (servant != null)
 				servant.Detach ();
 			Dispose ();
@@ -451,8 +423,6 @@ namespace Mono.Debugger
 		{
 			check_alive ();
 			servant.Stop ();
-			operation_completed_event.WaitOne ();
-			is_running = false;
 			flags |= Flags.AutoRun;
 		}
 
@@ -563,8 +533,6 @@ namespace Mono.Debugger
 		{
 			lock (this) {
 				check_alive ();
-				is_running = true;
-				operation_completed_event.Reset ();
 				RuntimeInvokeResult result = new RuntimeInvokeResult (this);
 				servant.RuntimeInvoke (
 					function, object_argument, param_objects,
@@ -821,8 +789,6 @@ namespace Mono.Debugger
 			if (servant != null) {
 				servant.Dispose ();
 				servant = null;
-
-				operation_completed_event.Set ();
 			}
 		}
 
@@ -862,7 +828,7 @@ namespace Mono.Debugger
 			get;
 		}
 
-		public abstract void Completed ();
+		internal abstract void Completed ();
 
 		public abstract void Abort ();
 
@@ -891,28 +857,43 @@ namespace Mono.Debugger
 			get { return thread.WaitHandle; }
 		}
 
+		internal override void Completed ()
+		{ }
+
 		public override void Abort ()
 		{
 			thread.Stop ();
 		}
+	}
 
-		public override void Completed ()
+	public class OperationCommandResult : ThreadCommandResult
+	{
+		protected ST.ManualResetEvent completed_event = new ST.ManualResetEvent (false);
+
+		internal OperationCommandResult (Thread thread)
+			: base (thread)
+		{ }
+
+		public override ST.WaitHandle CompletedEvent {
+			get { return completed_event; }
+		}
+
+		internal override void Completed ()
 		{
-			thread.SetCompleted ();
+			completed_event.Set ();
 		}
 	}
 
-	public class RuntimeInvokeResult : ThreadCommandResult
+	public class RuntimeInvokeResult : OperationCommandResult
 	{
 		internal RuntimeInvokeResult (Thread thread)
 			: base (thread)
 		{ }
 
-
 		public override void Abort ()
 		{
 			Thread.AbortInvocation (ID);
-			Thread.WaitHandle.WaitOne ();
+			completed_event.WaitOne ();
 		}
 
 		public long ID;

test/testsuite/TestNestedBreakStates.cs
@@ -50,6 +50,11 @@ namespace Mono.Debugger.Tests
 			AssertExecuteInBackground ("print -nested-break UnhandledException ()");
 			AssertNestedBreakState (thread, "X.UnhandledException()", GetLine ("unhandled"));
 
+			AssertPrint (thread, "catch",
+				     "(MyUnhandledException) { \"MyUnhandledException: Exception of type 'MyUnhandledException' " +
+				     "was thrown.\n  at X.UnhandledException () [0x00000] in " + FileName + ":" +
+				     GetLine ("unhandled") + " \" }");
+
 			AssertExecute ("continue");
 			e = AssertEvent (DebuggerEventType.CommandDone);
 			if (!(e.Data is ScriptingException))

commit 431b52e439a2a7ae517a5d62e7edc4b86f47e014

Fix a regression introduced in r136640: reset the 'operation_completed_event' when starting a new operation. 2009-06-20  Martin Baulig  <martin@ximian.com>
	* backend/SingleSteppingEngine.cs
	(SSE.StartOperation): Fix a regression introduced in r135640:
	reset `operation_completed_event' when starting a new operation.

svn path=/trunk/debugger/; revision=136613

backend/SingleSteppingEngine.cs
@@ -664,6 +664,7 @@ namespace Mono.Debugger.Backend
 
 				engine_stopped = false;
 				last_target_event = null;
+				operation_completed_event.Reset ();
 			}
 		}
 