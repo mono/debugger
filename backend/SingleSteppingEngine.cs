dfac350e5acf836d579586dce57f3ce43462fb50
Martin Baulig <martin@novell.com>
2009-08-05 22:22:53 +0000

    Reflect latest API changes.
    svn path=/branches/mono-2-4-2/debugger/; revision=139452

--- backend/SingleSteppingEngine.cs
+++ backend/SingleSteppingEngine.cs
@@ -1047,7 +1047,7 @@ namespace Mono.Debugger.Backend
 				return action;
 			}
 
-			foreach (ExceptionCatchPoint handle in exception_handlers.Values) {
+			foreach (ExceptionCatchPoint handle in process.Session.ExceptionCatchPoints) {
 				Report.Debug (DebugFlags.SSE,
 					      "{0} invoking exception handler {1} for {0}",
 					      this, handle.Name, exc);
